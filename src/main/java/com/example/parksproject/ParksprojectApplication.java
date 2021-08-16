e0da4684472fd4b6ff12cde5b144ebc48a69ddb4
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-08-16 23:45:49 +0900

    Initial commit

---
+++ src/main/java/com/example/parksproject/ParksprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.parksproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParksprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parksproject/ParksprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parksproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParksprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
