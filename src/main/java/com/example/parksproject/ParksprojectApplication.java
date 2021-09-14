25709fc2257a2522b31d0e15b706ef549d29bbf7
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-09-14 20:12:14 +0900

    query dsl 추가

--- src/main/java/com/example/parksproject/ParksprojectApplication.java
+++ src/main/java/com/example/parksproject/ParksprojectApplication.java
@@ -1,9 +1,13 @@
 package com.example.parksproject;
 
 import com.example.parksproject.config.AppProperties;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
@@ -13,4 +17,8 @@ public class ParksprojectApplication {
         SpringApplication.run(ParksprojectApplication.class, args);
     }
 
+    @Bean
+    JPAQueryFactory queryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
