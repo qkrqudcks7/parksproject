32799f96f6b6b229f1ff7f74bdc049ff66528a42
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-09-14 23:17:24 +0900

    query dsl 수정

--- src/main/java/com/example/parksproject/ParksprojectApplication.java
+++ src/main/java/com/example/parksproject/ParksprojectApplication.java
@@ -1,13 +1,10 @@
 package com.example.parksproject;
 
 import com.example.parksproject.config.AppProperties;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
-import javax.persistence.EntityManager;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
@@ -15,10 +12,5 @@ public class ParksprojectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ParksprojectApplication.class, args);
-    }
-
-    @Bean
-    JPAQueryFactory queryFactory(EntityManager em) {
-        return new JPAQueryFactory(em);
     }
 }
--- src/main/java/com/example/parksproject/controller/StudyController.java
+++ src/main/java/com/example/parksproject/controller/StudyController.java
@@ -31,7 +31,7 @@ public class StudyController {
     public ResponseEntity<?> getAllStudy() {
         List<Study> all = studyRepository.findAll();
         List<StudyResponse> collect = all.stream().map(
-                study -> new StudyResponse(study.getId(), study.getPath(), study.getTitle(), study.getShortDescription(), study.getLongDescription(), study.getImage(),study.getMembers(),study.getManagers(), study.getCategorys(), study.isRecruiting(), study.isPublished(), study.isClosed(), study.getMembersId(), study.getLocation(), study.getMaxMember()))
+                study -> new StudyResponse(study.getId(), study.getPath(), study.getTitle(), study.getShortDescription(), study.getLongDescription(), study.getImage(),study.getApplies(),study.getManagers(), study.getCategorys(), study.isRecruiting(), study.isPublished(), study.isClosed(), study.getMembersId(), study.getLocation(), study.getMaxMember()))
                 .collect(Collectors.toList());
 
         Collections.reverse(collect);
