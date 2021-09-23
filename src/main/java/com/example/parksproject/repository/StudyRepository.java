1bb25e459f3d2bb12dbed35fa139b715c4b9cdd5
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-09-23 20:08:34 +0900

    query dsl 삭제

--- src/main/java/com/example/parksproject/repository/StudyRepository.java
+++ src/main/java/com/example/parksproject/repository/StudyRepository.java
@@ -3,5 +3,5 @@ package com.example.parksproject.repository;
 import com.example.parksproject.domain.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudyRepository extends JpaRepository<Study,Long>, StudyRepositoryExtension {
+public interface StudyRepository extends JpaRepository<Study,Long> {
 }
